0581f8759a443922f07850fd0ecf0c710b251f6d
owenaled <oagriff@gmail.com>
2023-10-26 09:20:31 +0100

    Finished excercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let longString = "";
+    for (let i = 0; i < num; i++) {
+        longString = longString + string;
+      }
+      return longString;
 };
 
 // Do not edit below this line
